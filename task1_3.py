3b025668393190eafd2b1e47f53759e2fdfb0c98
Terekhin <blackrat72@qip.ru>
2023-07-14 21:37:03 +0300

    fix task3

--- task1_3.py
+++ task1_3.py
@@ -12,9 +12,9 @@ while True:
     else:
         count = 0
         for i in range(2, num // 2 + 1):
-            if (num % i == 0):
+            if num % i == 0:
                 count += 1
-        if (count == 0):
+        if count == 0:
             print("Число простое")
             break
         else:
